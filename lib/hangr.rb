e310820883ca262e376a52382ebf69a9e44f1a38
violetaria <terri.chu@outlook.com>
2015-10-26 14:38:43 -0400

    added activerecord associations

--- lib/hangr.rb
+++ lib/hangr.rb
@@ -6,10 +6,11 @@ module Hangr
   # Your code goes here...
 
   class Player < ActiveRecord::Base
+    has_many :games
   end
 
   class Game < ActiveRecord::Base
-    
+    belongs_to :player
   end
 end
 
